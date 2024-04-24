008560d5a02f3f482d81b1ac5988c02e9eab5053
sheetuchoudhary <161558244+sheetuchoudhary@users.noreply.github.com>
2024-04-24 16:03:03 +0000

    power1

--- pro_1.py
+++ pro_1.py
@@ -1,3 +1,13 @@
 '''
 problem_01
 '''
+#program for design a Power() function 
+def Power(number , power ):
+    if power == 0:
+        return 1
+    else:
+        return (number*Power(number,power-1))
+number = int(input('number : '))
+power = int(input('Power : '))
+result = (Power(number , power))
+print(result)
