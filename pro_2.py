7d2e57b2c353e480157e2684cb5bfa8b2e5b7c6e
sheetuchoudhary <161558244+sheetuchoudhary@users.noreply.github.com>
2024-04-25 14:04:41 +0000

    dd

--- pro_2.py
+++ pro_2.py
@@ -1,3 +1,35 @@
 '''
 problem_2
 '''
+#Winner Winner Chocolate Dinner
+'''def bowl(co_num):
+    return 
+co_num = int(input('number of chocolates : '))
+print(bowl(co_num))'''
+
+def Player_1(co_num):
+    co_num = co_num-1
+    return Player_2(co_num)
+def Player_2(co_num):
+    if co_num %2==0 :
+        co_num = co_num-2
+        return Player_1(co_num)
+    else :
+        co_num = co_num-1
+        return Player_1(co_num)
+        
+    
+
+#Winner Winner Chocolate Dinner
+def bowl(co_num):
+        one = Player_1(co_num)
+        if one <= 2:
+            print('Player2 win')
+            
+        else:
+            two = Player_2(co_num-1)
+            if two == 1 :
+                print('Player1 win')
+    
+co_num = int(input('number of chocolates : '))
+print(bowl(co_num))
